8a505c61e667d9614a25469e3c76579d181030c3
Luke Hendriks <66973798+l3keboy@users.noreply.github.com>
2024-12-16 12:31:01 +0100

    Update whyChooseUs.tsx

--- src/src/components/mainContent/whyChooseUs/whyChooseUs.tsx
+++ src/src/components/mainContent/whyChooseUs/whyChooseUs.tsx
@@ -10,7 +10,14 @@ export default function WhyChooseUs() {
       <ReasonComponent
         image_url="/img/stock_discord_image.png"
         title="Music & Radio"
-        description="Extend a warm and personalized greeting to new members when they join the server.Take the opportunity to introduce them to the server's rules, topics of discussion, and any ongoing events or activities. This way, you know that everyone has a positive and inclusive experience from the moment they join. Promote a sense of community and foster engagement among users. Make sure that new members feel valued and informed so they can participate and contribute to the vibrant and engaging channels you’ve created. Whether you’re all playing a game or joining a new guild together, or even just chatting in the servers, every member of each channel should feel like part of a community."
+        description="Advanced audio player with support to for Spotify, SoundCloud,
+              Deezer, Apple Music, Reddit, TikTok, Text-to-Speech and Radio
+              stations (with TuneIn)!Advanced audio player with support to for
+              Spotify, SoundCloud, Deezer, Apple Music, Reddit, TikTok,
+              Text-to-Speech and Radio stations (with TuneIn)! Advanced audio
+              player with support to for Spotify, SoundCloud, Deezer, Apple
+              Music, Reddit, TikTok, Text-to-Speech and Radio stations (with
+              TuneIn)!"
       />
       <ReasonComponent
         image_url="/img/stock_discord_image.png"
