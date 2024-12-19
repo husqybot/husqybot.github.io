59a52c94052758ddc97242dd3f0f2062209c46a2
Luke Hendriks <66973798+l3keboy@users.noreply.github.com>
2024-12-19 12:36:48 +0100

    Animate discover content section

--- src/src/components/mainContent/discover/discover.tsx
+++ src/src/components/mainContent/discover/discover.tsx
@@ -1,10 +1,71 @@
+import { useEffect } from "react";
+
 // Import components
 import DiscoverItem from "./discoverComponents/discoverItem/discoverItem";
 
+// Import functions
+import isElementInViewport from "../../../functions/isElementInViewport";
+
 // Import styles
 import styles from "./discover.module.css";
+import discoverItemStyles from "./discoverComponents/discoverItem/discoverItem.module.css";
 
 export default function Discover() {
+  // Event listeners
+  const checkElements = async () => {
+    var targetElements = document.getElementsByClassName(
+      discoverItemStyles.discover_item_container
+    );
+
+    if (isElementInViewport(targetElements[0])) {
+      targetElements[0].classList.add(
+        discoverItemStyles.animate_discover_container
+      );
+      targetElements[0].classList.add(discoverItemStyles.opacity_1);
+    }
+    if (isElementInViewport(targetElements[1])) {
+      targetElements[1].classList.add(
+        discoverItemStyles.animate_discover_container
+      );
+      targetElements[1].classList.add(discoverItemStyles.opacity_1);
+    }
+    if (isElementInViewport(targetElements[2])) {
+      targetElements[2].classList.add(
+        discoverItemStyles.animate_discover_container
+      );
+      targetElements[2].classList.add(discoverItemStyles.opacity_1);
+    }
+    if (isElementInViewport(targetElements[3])) {
+      targetElements[3].classList.add(
+        discoverItemStyles.animate_discover_container
+      );
+      targetElements[3].classList.add(discoverItemStyles.opacity_1);
+    }
+    if (isElementInViewport(targetElements[4])) {
+      targetElements[4].classList.add(
+        discoverItemStyles.animate_discover_container
+      );
+      targetElements[4].classList.add(discoverItemStyles.opacity_1);
+    }
+    if (isElementInViewport(targetElements[5])) {
+      targetElements[5].classList.add(
+        discoverItemStyles.animate_discover_container
+      );
+      targetElements[5].classList.add(discoverItemStyles.opacity_1);
+    }
+  };
+
+  useEffect(() => {
+    checkElements();
+    // Attach the event listener to the window
+    window.addEventListener("scroll", checkElements);
+
+    // Cleanup the event listener when the component unmounts or is re-rendered
+    return () => {
+      window.removeEventListener("scroll", checkElements);
+    };
+  }, []); // Empty dependency array ensures it runs only once, similar to componentDidMount
+
   return (
     <div className={styles.discover_container}>
       <DiscoverItem
