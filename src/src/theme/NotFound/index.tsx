8faea8676201ca37e10c2e95b597ce4e1763ac1e
Luke Hendriks <66973798+l3keboy@users.noreply.github.com>
2024-12-20 13:44:35 +0100

    Add custom 404 page styles

---
+++ src/src/theme/NotFound/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "@theme/Layout";
+import NotFoundContent from "@theme/NotFound/Content";
+
+export default function Index(): JSX.Element {
+  return (
+    <>
+      <Layout>
+        <NotFoundContent />
+      </Layout>
+    </>
+  );
+}
---
+++ src/static/index.tsx
@@ -0,0 +1,16 @@
+import React, { type ReactNode } from "react";
+import Layout from "@theme/Layout";
+
+// Import components
+import MainButton from "../../components/buttons/mainButton/mainButton";
+
+// Import styles
+import styles from "./styles.module.css";
+
+export default function NotFound(): ReactNode {
+  return (
+    <Layout>
+
+    </Layout>
+  );
+}
